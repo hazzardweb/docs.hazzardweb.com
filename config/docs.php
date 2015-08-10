187a0ce5d63b5dd09d5c728c9a6d5b00cacb4979
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2015-08-10 20:16:01 +0300

    adde image select

--- config/docs.php
+++ config/docs.php
@@ -30,6 +30,7 @@ return [
 	'manual_names' => [
 		'filepicker' 		  => 'Filepicker',
         'imagepicker'         => 'Image Picker',
+        'imageselect'         => 'Image Select',
 		'easylogin-pro' 	  => 'EasyLogin Pro',
 		'ajax-comment-system' => 'Ajax Comment System',
 	],
